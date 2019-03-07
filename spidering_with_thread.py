8f2d59f0cf032f0729814a0b58c8c260a7a47e70
MoonKuma <1244074267@qq.com>
2019-03-07 13:59:37 +0800

    finish testing the spidering with thread

--- spidering_with_thread.py
+++ spidering_with_thread.py
@@ -19,40 +19,72 @@ import gc
 import time
 import traceback
 from concurrent import futures
+from utils.clean_path import clean_target_path
 
 
 save_path = 'file_saved/zh.moegirl'
-root = 'https://zh.moegirl.org/%E7%99%BD%E5%AD%A6'
+clean_target_path(file_path=save_path, report=False)  # delete old results
 
-MAX_WEB_NUM = 1000  # the max num for computing
-SLEEP_TIME = 0.1
+root = 'https://zh.moegirl.org/%E7%99%BD%E5%AD%A6'
+# root = 'http://www.google.com'  # use google to test time out. Guess why? XD
+
+SLEEP_TIME = 0.5  # sleep if no job available
 MAX_WORKER = 10  # worker in threadpool executor
-PAGES_NUM = 100 # how many target pages are needed
+TIME_OUT_ACQUIRING = 5  # acquiring key time out
+TIME_OUT_HTTP = (2.0, 10.0)  # http connect/read time out
+PAGES_NUM = 10  # default pages num
 
 internal_links_result = list()  # links that have been computed
-waiting_list = WaitingList(root=root)  # links in waiting
-http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+waiting_list = WaitingList(root=root, timeout=TIME_OUT_ACQUIRING)  # links in waiting
+http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where(), timeout=urllib3.Timeout(connect=TIME_OUT_HTTP[0], read=TIME_OUT_HTTP[1]))
+
+info_dict = {'ROOT': root,
+             'SLEEP_TIME': SLEEP_TIME,
+             'MAX_WORKER': MAX_WORKER,
+             'TIME_OUT_ACQUIRING': TIME_OUT_ACQUIRING,
+             'TIME_OUT_HTTP': TIME_OUT_HTTP}
 
 
-def assign_work():
+def assign_work(report=False):
     # assign one unhandled work from the waiting list
     link = waiting_list.pop_link()
     msg = 'Link:' + link + ', assigned to worker'
     print(msg)
     if link != "":
         # compute here
-        do_work(link)
-        msg = 'Link:' + link + ', Finished!'
-        print(msg)
-        return 1
+        result = do_work(link)
+        msg = 'Link:' + link
+        if result == 1:
+            msg += ', Finished!'
+        if result == 0:
+            msg += ', Time Out!'
+        if result == -1:
+            msg += ', Error!'
+        if report:
+            print(msg)
+        return result
     else:
+        msg = 'Waiting list is empty! Wait for another ' + str(SLEEP_TIME)
+        print(msg)
         time.sleep(SLEEP_TIME)
-        return 0
+        return -2
 
 
 def do_work(link):
     internal_links = get_current_internal(link, save_path)
-    waiting_list.add_links(internal_links)
+    if internal_links is not None:
+        if len(internal_links)>0:
+            if internal_links[0] not in [0, -1]:
+                # standard
+                waiting_list.add_links(internal_links)
+                return 1
+            else:
+                # time out
+                return internal_links[0]
+        else:
+            return 1
+    else:
+        return -1
 
 
 # to compute
@@ -64,14 +96,20 @@ def get_current_internal(current_link, save_path, path_only=True):
         parsed = parse.urlparse(current_link)
         link_path = parsed.scheme + '://' + parsed.netloc + parsed.path
     internal_links_result.append(link_path)
-    response = http.request('get', link_path)
     internal_links_tmp = list()
-    if response.status == 200:  # succeed in connection
-        bs_obj = BeautifulSoup(response.data, "html.parser")
-        content = read_content(bs_obj=bs_obj)
-        save_content(file_path=save_path, content=content, url=link_path)
-        internal_links_tmp = get_links_inside(bs_obj=bs_obj, url=link_path, path_only=path_only)
-    return internal_links_tmp
+    try:
+        response = http.request('get', link_path) # here may throw a time out exception
+        if response.status == 200:  # succeed in connection
+            bs_obj = BeautifulSoup(response.data, "html.parser")
+            content = read_content(bs_obj=bs_obj)
+            save_content(file_path=save_path, content=content, url=link_path)
+            internal_links_tmp = get_links_inside(bs_obj=bs_obj, url=link_path, path_only=path_only) # here may return none if the pages is to large to read
+            return internal_links_tmp
+    except urllib3.exceptions.MaxRetryError:
+        return [0, current_link]  # [0] stands for time out after max retry
+    except:
+        return [-1, current_link]  # [-1] stands for other type of error
+
 
 
 # test
@@ -79,9 +117,9 @@ def test(test_times=PAGES_NUM):
     time_0 = time.time()
     for i in range(0, test_times):
         assign_work()
-    print('Marked:', waiting_list.get_length_marking())
-    print('Waiting:', waiting_list.get_length_waiting())
-    print('Time cost: ', time.time()-time_0)
+    info_dict['Marked'] = waiting_list.get_length_marking()
+    info_dict['Waiting'] = waiting_list.get_length_waiting()
+    info_dict['Time cost'] = time.time() - time_0
 
 
 # test inside threadpool
@@ -91,13 +129,41 @@ def test_threadpool(test_times=PAGES_NUM):
     # point to start
     time_0 = time.time()
     assign_work()
-    for i in range(0, test_times-1):
+    for i in range(0, test_times):
         future = executor.submit(fn=assign_work)
         future_list.append(future)
     futures.wait(future_list)
-    print('Marked:', waiting_list.get_length_marking())
-    print('Waiting:', waiting_list.get_length_waiting())
-    print('Time cost: ', time.time() - time_0)
+    info_dict['Test times'] = test_times
+    info_dict['Marked'] = waiting_list.get_length_marking()
+    info_dict['Waiting'] = waiting_list.get_length_waiting()
+    info_dict['Result length'] = len(future_list)
+    result_dict = {'Success': 0, 'TimeOut': 0, 'Error': 0, 'No Job':0}
+    for future in future_list:
+        if future.result() == 1:
+            result_dict['Success'] = result_dict['Success'] + 1
+        if future.result() == 0:
+            result_dict['TimeOut'] = result_dict['TimeOut'] + 1
+        if future.result() == -1:
+            result_dict['Error'] = result_dict['Error'] + 1
+        if future.result() == -2:
+            result_dict['No Job'] = result_dict['No Job'] + 1
+    info_dict['Result'] = result_dict
+    info_dict['Time cost'] = time.time() - time_0
+    save_info()
 
-test()
+
+# save info
+def save_info(report=True):
+    if report:
+        for item in info_dict.items():
+            print(item)
+    file_loc = os.path.join(save_path, 'info')
+    if not os.path.exists(file_loc):
+        os.mkdir(file_loc)
+    file_name = os.path.join(file_loc, 'info.txt')
+    with open(file_name, 'w') as file_w:
+        file_w.write(str(info_dict))
+
+# test()
+test_threadpool(100000)
 
